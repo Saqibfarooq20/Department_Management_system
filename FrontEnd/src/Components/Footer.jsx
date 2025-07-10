89d9019486a69a48a490bd7aadc329d4ec533dc6
private-me8941 <deepanshi.varshney403099@gmail.com>
2025-07-10 15:25:36 +0530

    chl ja

--- FrontEnd/src/Components/Footer.jsx
+++ FrontEnd/src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import '../Styles/Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 IMCS Department Management System. All rights reserved.</p>
+      <p>&copy; 2025 IMCS Department Management System. All rights reserved dd.</p>
     </footer>
   );
 };
